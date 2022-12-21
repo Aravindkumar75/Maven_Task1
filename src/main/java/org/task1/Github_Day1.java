71bcc00e5fc4f9b91b602dac22245ef616745327
Aravindkumar75 <contactvaravind@gmail.com>
2022-12-21 16:09:10 +0530

    Github_Day1

--- src/main/java/org/task1/Github_Day1.java
+++ src/main/java/org/task1/Github_Day1.java
@@ -5,8 +5,8 @@ public class Github_Day1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Github");
-		System.out.println("Maven");
-		System.out.println("DATA DRIVEN");
+		System.out.println("Framework");
+		System.out.println("POM");
 	}
 
 }
